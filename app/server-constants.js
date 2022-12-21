ad2a2a8d6be9a7ca7a93167cd3c6c4ce684913d9
n0bisuke <rsksound@gmail.com>
2022-12-21 12:23:57 +0900

    deploy

--- app/server-constants.js
+++ app/server-constants.js
@@ -7,8 +7,8 @@ const NOTION_API_SECRET = process.env.NOTION_API_SECRET
 const DATABASE_ID = process.env.DATABASE_ID
 const NEXT_PUBLIC_URL = process.env.NEXT_PUBLIC_URL
 const NEXT_PUBLIC_GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID
-const NEXT_PUBLIC_SITE_TITLE = 'easy-notion-blog'
-const NEXT_PUBLIC_SITE_DESCRIPTION = 'Easy to start your blog. You can write on your Notion.'
+const NEXT_PUBLIC_SITE_TITLE = 'のびすけブログ'
+const NEXT_PUBLIC_SITE_DESCRIPTION = '日記とかも入れてみようと思います'
 const BLOG_INDEX_CACHE = path.resolve('.blog_index_data')
 const NUMBER_OF_POSTS_PER_PAGE = 10
 
