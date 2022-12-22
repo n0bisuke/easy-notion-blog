bf090db268780e48e59015b3f1453555598dd4d0
n0bisuke <rsksound@gmail.com>
2022-12-22 12:06:29 +0900

    deploy

--- app/posts/head.tsx
+++ app/posts/head.tsx
@@ -1,7 +1,8 @@
 import DocumentHead from '../../components/document-head'
 
 const BlogHead = () => (
-  <DocumentHead title="Blog" path="/blog" />
+  // <DocumentHead title="Blog" path="/blog" />
+  <DocumentHead title="Blog" path="/posts" />
 )
 
 export default BlogHead
