de67c8ba871716054bfc1a24ab99f42f953d702d
n0bisuke <rsksound@gmail.com>
2022-12-22 13:28:29 +0900

    deploy

--- app/server-constants.js
+++ app/server-constants.js
@@ -5,12 +5,14 @@ const path = require('path')
 
 const NOTION_API_SECRET = process.env.NOTION_API_SECRET
 const DATABASE_ID = process.env.DATABASE_ID
-const NEXT_PUBLIC_URL = process.env.NEXT_PUBLIC_URL || `https://nobi.suke.dev`
+const NEXT_PUBLIC_URL = process.env.NEXT_PUBLIC_URL
 const NEXT_PUBLIC_GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID
 const NEXT_PUBLIC_SITE_TITLE = 'のびすけブログ'
 const NEXT_PUBLIC_SITE_DESCRIPTION = '日記とかも入れてみようと思います。'
 const BLOG_INDEX_CACHE = path.resolve('.blog_index_data')
 const NUMBER_OF_POSTS_PER_PAGE = 10
+
+// console.log(`test`,NEXT_PUBLIC_GA_TRACKING_ID);
 
 module.exports = {
   NOTION_API_SECRET,
