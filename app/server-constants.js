57df69b8c703e65af6fb6eb6082579b7d6b94450
otoyo <hiroki.t.1988@gmail.com>
2023-01-02 17:22:29 +0900

    9317e5e54ee10d6588a5e5ab0d81837f87835c9e マージ

--- app/server-constants.js
+++ app/server-constants.js
@@ -1,18 +1,16 @@
 /* eslint @typescript-eslint/no-var-requires: 0 */
 // use commonjs so it can be required without transpiling
 
-const path = require('path')
-
 const NOTION_API_SECRET = process.env.NOTION_API_SECRET
 const DATABASE_ID = process.env.DATABASE_ID
+
 const NEXT_PUBLIC_URL = process.env.NEXT_PUBLIC_URL || `https://nobi.suke.dev`
 const NEXT_PUBLIC_GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID || `G-4EVC6VE7DV`
 const NEXT_PUBLIC_SITE_TITLE = 'のびすけブログ'
 const NEXT_PUBLIC_SITE_DESCRIPTION = '日記とかも入れてみようと思います'
-const BLOG_INDEX_CACHE = path.resolve('.blog_index_data')
+
 const NUMBER_OF_POSTS_PER_PAGE = 10
 // console.log(`test`,NEXT_PUBLIC_GA_TRACKING_ID, NEXT_PUBLIC_URL);
-
 
 module.exports = {
   NOTION_API_SECRET,
@@ -21,6 +19,5 @@ module.exports = {
   NEXT_PUBLIC_GA_TRACKING_ID,
   NEXT_PUBLIC_SITE_TITLE,
   NEXT_PUBLIC_SITE_DESCRIPTION,
-  BLOG_INDEX_CACHE,
   NUMBER_OF_POSTS_PER_PAGE,
 }
