6113303dae2782d0ccc36a7351ce890fef0986d3
n0bisuke <rsksound@gmail.com>
2023-01-10 18:42:48 +0900

    #159反映

--- app/server-constants.js
+++ app/server-constants.js
@@ -8,7 +8,7 @@ const DATABASE_ID = process.env.DATABASE_ID
 const NEXT_PUBLIC_URL = process.env.NEXT_PUBLIC_URL || `https://nobi.suke.dev`
 const NEXT_PUBLIC_GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID || `G-4EVC6VE7DV`
 const NEXT_PUBLIC_SITE_TITLE = 'のびすけブログ'
-const NEXT_PUBLIC_SITE_DESCRIPTION = '日記とかも入れてみようと思います。'
+const NEXT_PUBLIC_SITE_DESCRIPTION = '日記とかも入れてみようと思います'
 const BLOG_INDEX_CACHE = path.resolve('.blog_index_data')
 const NUMBER_OF_POSTS_PER_PAGE = 10
 // console.log(`test`,NEXT_PUBLIC_GA_TRACKING_ID, NEXT_PUBLIC_URL);
